c7ac93f985063bfc7459c38c77d434912efcfa24
vagrant <vagrant@precise32.(none)>
2021-02-15 18:29:04 +0000

    square nb 2

--- 0x0B-python-inheritance/11-square.py
+++ 0x0B-python-inheritance/11-square.py
@@ -8,3 +8,31 @@
 """
 
 Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Represents a class called Square with a private instance
+    attribute called size
+    """
+
+    def __init__(self, size):
+        """
+        Initialization of the private instance attribute
+        Size will be validated using the integer_validator
+        implemented in the base class
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Function that returns the area of the Square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        Function str that defines the string representation of the Square
+        """
+        return "[Square] {}/{}".format(str(self.__size),
+                                       str(self.__size))
