601f2b18bbede9e0fb3ab1373cbd801409af27bd
vagrant <vagrant@precise32.(none)>
2021-02-15 18:23:35 +0000

    square nb 1

---
+++ 0x0B-python-inheritance/10-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    9-rectangle.py
+    Module that defines a class called Rectangle that inherits from
+    class BaseGeometry and returns its width and height
+    Including a method to calculate the Rectangle's area
+    Including __str__ method to represent the Rectangle
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
